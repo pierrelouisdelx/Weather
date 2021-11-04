53d9c0f7096e75205bd41e0b3bbaafcde3835bd6
Pierre-Louis Delcroix <you@example.com>
2021-11-04 01:42:26 +0100

    loading cities from async storage working

--- screens/Landing.js
+++ screens/Landing.js
@@ -1,13 +1,27 @@
 import React,  { useEffect, useState } from 'react';
-import { StyleSheet, FlatList, ImageBackground, AsyncStoragei, TextInput, View} from 'react-native';
-
+import { StyleSheet, FlatList, ImageBackground, TextInput, View} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import CityCard from '../components/CityCard';
-
-cities = ['Tokyo']
 
 function search(city)
 {
     fetch(`http://192.168.1.93:3000/search/${city}`);
+    AsyncStorage.getItem('cities', (err, result) => {
+        if (result !== null) {
+            var cities = JSON.parse(result);
+            var found = 0;
+            for (let i = 0; i < cities.length; i++)
+                if (cities[i] == city)
+                    found = 1
+            if (!found)
+            {
+                cities.push(city);
+                AsyncStorage.setItem('cities', JSON.stringify(cities));
+            }
+        } else {
+            AsyncStorage.setItem('cities', JSON.stringify([city]));
+        }
+    });
 }
 
 export default LandingScreen = ({ navigation }) => {
@@ -15,19 +29,23 @@ export default LandingScreen = ({ navigation }) => {
     const [text, setText] = useState('');
 
     useEffect(() => {
-        fetch("http://192.168.1.93:3000/city", {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                cities: cities,
-            })
-        })
-        .then((response) => response.json())
-        .then((json) => setData(json))
-        .catch((error) => console.error(error))
+        AsyncStorage.getItem('cities')
+            .then(value => JSON.parse(value))
+            .then(cities =>
+                fetch("http://192.168.1.93:3000/city", {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        cities: cities,
+                    })
+                })
+                .then((response) => response.json())
+                .then((json) => setData(json))
+                .catch((error) => console.error(error))
+            )
     }, []);
 
     return (
